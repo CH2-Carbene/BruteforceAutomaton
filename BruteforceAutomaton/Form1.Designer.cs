1498f47ea6566eb53b7e3961645a572ebdb47041
CH2 <ch2-carbene@github.com>
2021-06-24 19:32:05 +0800

    前端完成

--- BruteforceAutomaton/Form1.Designer.cs
+++ BruteforceAutomaton/Form1.Designer.cs
@@ -41,6 +41,9 @@ namespace BruteforceAutomaton
             this.listView1 = new System.Windows.Forms.ListView();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.r = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.c = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.data = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -59,7 +62,7 @@ namespace BruteforceAutomaton
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -70,16 +73,15 @@ namespace BruteforceAutomaton
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer3);
-            this.splitContainer1.Size = new System.Drawing.Size(600, 360);
-            this.splitContainer1.SplitterDistance = 110;
-            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 137;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainer2.Name = "splitContainer2";
             // 
             // splitContainer2.Panel1
@@ -93,36 +95,34 @@ namespace BruteforceAutomaton
             this.splitContainer2.Panel2.Controls.Add(this.label2);
             this.splitContainer2.Panel2.Controls.Add(this.textBox2);
             this.splitContainer2.Panel2.Controls.Add(this.button2);
-            this.splitContainer2.Size = new System.Drawing.Size(600, 110);
-            this.splitContainer2.SplitterDistance = 300;
-            this.splitContainer2.SplitterWidth = 3;
+            this.splitContainer2.Size = new System.Drawing.Size(800, 137);
+            this.splitContainer2.SplitterDistance = 400;
             this.splitContainer2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.Size = new System.Drawing.Size(82, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "查找范围串";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(2, 23);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(3, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(226, 86);
+            this.textBox1.Size = new System.Drawing.Size(300, 106);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(232, 50);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(309, 62);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 29);
+            this.button1.Size = new System.Drawing.Size(88, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "浏览文件";
             this.button1.UseVisualStyleBackColor = true;
@@ -131,28 +131,27 @@ namespace BruteforceAutomaton
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(2, 7);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(3, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.Size = new System.Drawing.Size(82, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "查找内容串";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(4, 23);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(5, 29);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(213, 83);
+            this.textBox2.Size = new System.Drawing.Size(283, 103);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(221, 50);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(295, 62);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(73, 29);
+            this.button2.Size = new System.Drawing.Size(97, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "浏览文件";
             this.button2.UseVisualStyleBackColor = true;
@@ -162,7 +161,7 @@ namespace BruteforceAutomaton
             // 
             this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer3.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainer3.Name = "splitContainer3";
             // 
             // splitContainer3.Panel1
@@ -173,53 +172,71 @@ namespace BruteforceAutomaton
             // 
             this.splitContainer3.Panel2.Controls.Add(this.checkedListBox1);
             this.splitContainer3.Panel2.Controls.Add(this.button3);
-            this.splitContainer3.Size = new System.Drawing.Size(600, 247);
-            this.splitContainer3.SplitterDistance = 471;
-            this.splitContainer3.SplitterWidth = 3;
+            this.splitContainer3.Size = new System.Drawing.Size(800, 309);
+            this.splitContainer3.SplitterDistance = 628;
             this.splitContainer3.TabIndex = 0;
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.r,
+            this.c,
+            this.data});
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(0, 0);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(471, 247);
+            this.listView1.Size = new System.Drawing.Size(628, 309);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.CheckOnClick = true;
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
             "允许通配符",
             "不区分大小写",
             "全字匹配",
             "允许重复"});
-            this.checkedListBox1.Location = new System.Drawing.Point(19, 14);
-            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkedListBox1.Location = new System.Drawing.Point(25, 18);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(91, 180);
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 224);
             this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(19, 198);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(25, 248);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(91, 38);
+            this.button3.Size = new System.Drawing.Size(121, 48);
             this.button3.TabIndex = 0;
             this.button3.Text = "开始匹配！";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // r
+            // 
+            this.r.Text = "行";
+            // 
+            // c
+            // 
+            this.c.Text = "列";
+            // 
+            // data
+            // 
+            this.data.Text = "上下文";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.splitContainer1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "暴搜自动机文本查找软件";
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -254,6 +271,9 @@ namespace BruteforceAutomaton
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader r;
+        private System.Windows.Forms.ColumnHeader c;
+        private System.Windows.Forms.ColumnHeader data;
     }
 }
 
--- BruteforceAutomaton/Form1.cs
+++ BruteforceAutomaton/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,18 +15,277 @@ namespace BruteforceAutomaton
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog fileDialog;
+        bool clf=false;
         public Form1()
         {
             InitializeComponent();
+            fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = @"C:\Users\CH2\Documents\大三下\软工实验\mywork\text";
+            fileDialog.RestoreDirectory = true;
+            fileDialog.Title = "选择文件";
+            fileDialog.Filter = "所有文件(*.*)|*.*";
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+            /*OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "C:\\";
+
+            openFileDialog1.Filter = "文本文件 (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+
+                //label1.Text = openFileDialog1.FileName;
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+
+                textBox1.Text = sr.ReadToEnd();
+
+                sr.Close();
+
+            }*/
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                string file = fileDialog.FileName; 
+                System.IO.StreamReader sr = new System.IO.StreamReader(fileDialog.FileName);
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;
+                System.IO.StreamReader sr = new System.IO.StreamReader(fileDialog.FileName);
+                textBox2.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            
+            if (clf && e.CurrentValue == CheckState.Indeterminate)
+            {
+                e.NewValue = CheckState.Indeterminate;
+            }
+            
+            if (e.Index == 0)
+            {
+                if (e.CurrentValue==CheckState.Unchecked)
+                {
+                    /*checkedListBox1.SetItemChecked(1, false);
+                    checkedListBox1.SetItemChecked(2, false);
+                    checkedListBox1.SetItemChecked(3, false);*/
+                    clf = true;
+                    checkedListBox1.SetItemCheckState(1, CheckState.Indeterminate);
+                    checkedListBox1.SetItemCheckState(2, CheckState.Indeterminate);
+                    checkedListBox1.SetItemCheckState(3, CheckState.Indeterminate);
+                    //checkedListBox1.
+                }
+                else if(e.CurrentValue==CheckState.Checked)
+                {
+                    clf = false;
+                    checkedListBox1.SetItemCheckState(1, CheckState.Unchecked);
+                    checkedListBox1.SetItemCheckState(2, CheckState.Unchecked);
+                    checkedListBox1.SetItemCheckState(3, CheckState.Unchecked);
+                }
+            }
+            //else if (!checkedListBox1.GetItemChecked(0) && e.CurrentValue == CheckState.Indeterminate)
+            //{
+            //    e.NewValue = CheckState.Indeterminate;
+            //}
+        }
+
+        public static int readInt(Byte[] buffer, ref int offset)
+        {
+            Int32 value = BitConverter.ToInt32(buffer, offset);
+            offset += 4;
+            return value;
+        }
+        public static void write(Byte[] buffer, int value, ref int offset)
+        {
+            byte[] bTemp = BitConverter.GetBytes(value);
+            for (Int32 i = 0; i < bTemp.Length; i++)
+            {
+                buffer[offset++] = bTemp[i];
+            }
+        }
+        public static void write(Byte[] buffer, bool value, ref int offset)
+        {
+            byte[] bTemp = BitConverter.GetBytes(value);
+            for (Int32 i = 0; i < bTemp.Length; i++)
+            {
+                buffer[offset++] = bTemp[i];
+            }
+        }
+        public static void write(Byte[] buffer, String value, ref Int32 offset)
+        {
+            if (!String.IsNullOrEmpty(value))
+            {
+                byte[] bTemp = Encoding.ASCII.GetBytes(value);
+
+                write(buffer, bTemp.Length, ref offset);
+                for (Int32 i = 0; i < bTemp.Length; i++)
+                {
+                    buffer[offset++] = bTemp[i];
+                }
+            }
+            else
+            {
+                write(buffer, 0, ref offset);
+            }
+        }
+        private byte[] getBytes()
+        {
+            string charBuf1 = textBox1.Text;
+            string charBuf2 = textBox2.Text;
+            
+            byte[] buffer=new byte[4+4+charBuf1.Length+4+charBuf2.Length];
+            int offset = 0;
+            write(buffer, checkedListBox1.GetItemChecked(0), ref offset);
+            write(buffer, checkedListBox1.GetItemChecked(1), ref offset);
+            write(buffer, checkedListBox1.GetItemChecked(2), ref offset);
+            write(buffer, checkedListBox1.GetItemChecked(3), ref offset);
+
+            //write(buffer, charBuf1.Length, ref offset);
+            write(buffer, charBuf1, ref offset);
+            //write(buffer, charBuf2.Length, ref offset);
+            write(buffer, charBuf2, ref offset);
+            return buffer;
+        }
+        private byte[] getByteResult()
+        {
+            String IP = "127.0.0.1";
+            int port = 8885;
+            IPAddress ip = IPAddress.Parse(IP);
+            Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            ClientSocket.Connect(endPoint);
+            Console.WriteLine("开始发送消息");
+            byte[] message = getBytes();
+            ClientSocket.Send(message);
+            Console.WriteLine("发送消息为:" + Encoding.ASCII.GetString(message));
+            byte[] receive = new byte[1024];
+            Console.WriteLine("接收消息为：" + Encoding.ASCII.GetString(receive));
+            ClientSocket.Close();
+            return receive;
+        }
+        private byte[] _getByteResult()
+        {
+            BinaryReader bw = new BinaryReader(new FileStream(@"C:\Users\CH2\Documents\大三下\软工实验\mywork\text\in.txt",
+FileMode.Open));
+            byte[] bt=bw.ReadBytes(1024);
+            bw.Close();
+            Console.WriteLine("接收消息为：" +Encoding.ASCII.GetString(bt));
+            return bt;
+        }
+        private Tuple<int,int>[] _getReveiveArray()
+        {
+            Tuple<int, int>[] c= new Tuple<int, int>[2];
+
+            c[0] = new Tuple<int, int>(6, 7);
+            c[1] = new Tuple<int, int>(8,9);
+           
+            return c;
+        }
+        private Tuple<int, int>[] getReveiveArray()
+        {
+            int offset = 0;
+            byte[] bs=_getByteResult();
+            int len=readInt(bs,ref offset);
+            Tuple<int, int>[] res=new Tuple<int, int>[len];
+            for(int i = 0; i < len; ++i) {
+                int x = readInt(bs, ref offset);
+                int y = readInt(bs, ref offset);
+                res[i] = new Tuple<int, int>(x,y);
+            }
+            return res;
+        }
+        private Tuple<int,int,String>[] _getFindArray(Tuple<int, int>[] offArray)
+        {
+            return new Tuple<int, int, string>[2] {
+                new Tuple<int, int, string>(1,2,"12"),
+                new Tuple<int, int, string>(3,4,"34")
+            };
+        }
+        private Tuple<int, int, String>[] getFindArray(Tuple<int, int>[] offArray)
+        {
+            int showContextLen = 3;
+            Tuple<int, int, String>[] res=new Tuple<int, int, String>[offArray.Length];
+            int offset = 0,nowline=0;
+            //foreach(Tuple<int, int> t in offArray)
+            for(int i=0;i< offArray.Length;++i)
+            {
+                Tuple<int, int> t = offArray[i];
+                while (offset+ textBox1.Lines[nowline].Length + 2 <= t.Item1)
+                {
+                    offset += textBox1.Lines[nowline].Length+2;
+                    nowline++;
+                }
+                int r = nowline + 1, c = t.Item1 - offset + 1;
+                string s= textBox1.Text.Substring(t.Item1,t.Item2-t.Item1);
+                int spreLen = Math.Min(c-1,showContextLen);
+                string spre= textBox1.Text.Substring(t.Item1-spreLen,spreLen);
+                if (c > showContextLen+1) spre = "..." + spre;
+
+                Tuple<int, int, String> nt = new Tuple<int, int, String>(r,c,spre+s+"...");
+                
+                res[i]=nt;
+            }
+            /*for (int i = 0; i < offArray.Length; ++i)
+            {
+                Tuple<int, int> t = offArray[i];
+                while (offset + textBox1.Lines[nowline].Length + 2 <= t.Item2-1)
+                {
+                    offset += textBox1.Lines[nowline].Length + 2;
+                    nowline++;
+                }
+                int r = nowline + 1, c = t.Item2 - offset + 1;
+
+                int stailLen = Math.Min(textBox1.Lines[nowline].Length - c, showContextLen);
+                string stail = textBox1.Text.Substring(t.Item2 + stailLen, stailLen);
+                if (c+ showContextLen < textBox1.Lines[nowline].Length) stail += "...";
+
+                //Tuple<int, int, String> nt = new Tuple<int, int, String>(r, c, spre + s);
+
+                res[i].Item3+=stail;
+            }*/
+
+
+            return res;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BinaryWriter bw = new BinaryWriter(new FileStream(@"C:\Users\CH2\Documents\大三下\软工实验\mywork\text\out.txt",
+            FileMode.Create));
+            bw.Write(getBytes());
+            bw.Close();
+
+            foreach(string s in textBox1.Lines){
+                Console.WriteLine(s.Length);
+            }
+
+            Tuple<int, int>[] reveiveArray= getReveiveArray();
+            Tuple<int, int, String>[] findArray=getFindArray(reveiveArray);
+            listView1.Items.Clear();
+            foreach (Tuple<int, int, String> fd in findArray)
+            {
+                ListViewItem lvi = new ListViewItem();
+
+                lvi.Text = fd.Item1.ToString();
+                lvi.SubItems.Add(fd.Item2.ToString());
+                lvi.SubItems.Add(fd.Item3);
+                listView1.Items.Add(lvi);
+            }
 
         }
     }
